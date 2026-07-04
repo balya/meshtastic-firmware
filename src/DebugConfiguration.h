meshtastic/firmware

feat(esp32): add native Ethernet support

src/DebugConfiguration.h
@@ -152,16 +152,18 @@ extern "C" void logLegacy(const char *level, const char *fmt, ...);
 // Default Bluetooth PIN
 #define defaultBLEPin 123456
 
-#if HAS_ETHERNET && defined(USE_CH390D)
+#if HAS_ETHERNET
+#if defined(USE_CH390D)
 #include <ESP32_CH390.h>
-#elif HAS_ETHERNET && !defined(USE_WS5500)
-#include <RAK13800_W5100S.h>
-#endif // HAS_ETHERNET
-
-#if HAS_ETHERNET && defined(USE_WS5500)
+#elif defined(USE_WS5500)
 #include <ETHClass2.h>
 #define ETH ETH2
-#endif // HAS_ETHERNET
+#elif defined(ESP32) && defined(ETH_PHY_TYPE)
+#include <ETH.h>
+#else
+#include <RAK13800_W5100S.h>
+#endif
+#endif
 
 #if HAS_WIFI
 #include <WiFi.h>

src/mesh/api/ethServerAPI.h
@@ -1,28 +1,45 @@
 #pragma once
 
+#include "configuration.h"
+
 #include "ServerAPI.h"
-#if !defined(USE_WS5500) && !defined(USE_CH390D)
+
+#if HAS_ETHERNET && !defined(USE_WS5500) && !defined(USE_CH390D)
+#if defined(ESP32) && defined(ETH_PHY_TYPE)
+#include <ETH.h>
+#include <WiFi.h>
+typedef WiFiClient MeshEthernetClient;
+typedef WiFiServer MeshEthernetServer;
+#else
 #include <RAK13800_W5100S.h>
+typedef EthernetClient MeshEthernetClient;
+typedef EthernetServer MeshEthernetServer;
+#endif
 
 /**
  * Provides both debug printing and, if the client starts sending protobufs to us, switches to send/receive protobufs
  * (and starts dropping debug printing - FIXME, eventually those prints should be encapsulated in protobufs).
  */
-class ethServerAPI : public ServerAPI<EthernetClient>
+class ethServerAPI : public ServerAPI<MeshEthernetClient>
 {
   public:
-    explicit ethServerAPI(EthernetClient &_client);
+    explicit ethServerAPI(MeshEthernetClient &_client);
 };
 
 /**
  * Listens for incoming connections and does accepts and creates instances of EthernetServerAPI as needed
  */
-class ethServerPort : public APIServerPort<ethServerAPI, EthernetServer>
+class ethServerPort : public APIServerPort<ethServerAPI, MeshEthernetServer>
 {
   public:
     explicit ethServerPort(int port);
 };
 
+void initEthApiServer(int port = SERVER_API_DEFAULT_PORT);
+void deInitEthApiServer();
+
+#if !HAS_WIFI
 void initApiServer(int port = SERVER_API_DEFAULT_PORT);
 void deInitApiServer();
+#endif
 #endif

src/mesh/wifi/WiFiAPClient.h
@@ -9,10 +9,17 @@
 #include <WiFi.h>
 #endif
 
-#if HAS_ETHERNET && defined(USE_WS5500)
+#if HAS_ETHERNET
+#if defined(ESP32) && defined(ETH_PHY_TYPE)
+#include <ETH.h>
+#elif defined(USE_WS5500)
 #include <ETHClass2.h>
 #define ETH ETH2
-#endif // HAS_ETHERNET
+#elif defined(USE_CH390D)
+#include <ESP32_CH390.h>
+#define ETH CH390
+#endif
+#endif
 
 extern bool needReconnect;
 extern concurrency::Periodic *wifiReconnect;
@@ -26,7 +33,7 @@ bool isWifiAvailable();
 
 uint8_t getWifiDisconnectReason();
 
-#if defined(USE_WS5500) || defined(USE_CH390D)
+#if HAS_ETHERNET && (defined(USE_WS5500) || defined(USE_CH390D) || defined(ETH_PHY_TYPE))
 // Startup Ethernet
 bool initEthernet();
 #endif

balya/meshtastic-firmware

src/DebugConfiguration.h
@@ -152,17 +152,20 @@ extern "C" void logLegacy(const char *level, const char *fmt, ...);
 // Default Bluetooth PIN
 #define defaultBLEPin 123456
 
-#if HAS_ETHERNET && defined(USE_ARDUINO_ETHERNET)
+#if HAS_ETHERNET
+#if defined(USE_ARDUINO_ETHERNET)
 #include <Ethernet.h> // arduino-libraries/Ethernet - supports W5500 auto-detect
-#elif HAS_ETHERNET && defined(USE_CH390D)
+#elif defined(USE_CH390D)
 #include <ESP32_CH390.h>
-#elif HAS_ETHERNET && !defined(USE_WS5500)
-#include <RAK13800_W5100S.h>
-#endif // HAS_ETHERNET
-
-#if HAS_ETHERNET && defined(ARCH_ESP32)
+#elif defined(USE_WS5500)
+#include <ETHClass2.h>
+#define ETH ETH2
+#elif defined(ARCH_ESP32) && defined(ETH_PHY_TYPE)
 #include <ETH.h>
-#endif // HAS_ETHERNET
+#else
+#include <RAK13800_W5100S.h>
+#endif
+#endif
 
 #if HAS_WIFI
 #include <WiFi.h>

src/mesh/api/ethServerAPI.h
@@ -1,32 +1,49 @@
 #pragma once
 
+#include "configuration.h"
+
 #include "ServerAPI.h"
-#if !defined(USE_WS5500) && !defined(USE_CH390D)
-#if defined(USE_ARDUINO_ETHERNET)
+
+#if HAS_ETHERNET && !defined(USE_WS5500) && !defined(USE_CH390D)
+#if defined(ARCH_ESP32) && defined(ETH_PHY_TYPE)
+#include <ETH.h>
+#include <WiFi.h>
+typedef WiFiClient MeshEthernetClient;
+typedef WiFiServer MeshEthernetServer;
+#elif defined(USE_ARDUINO_ETHERNET)
 #include <Ethernet.h>
+typedef EthernetClient MeshEthernetClient;
+typedef EthernetServer MeshEthernetServer;
 #else
 #include <RAK13800_W5100S.h>
+typedef EthernetClient MeshEthernetClient;
+typedef EthernetServer MeshEthernetServer;
 #endif
 
 /**
  * Provides both debug printing and, if the client starts sending protobufs to us, switches to send/receive protobufs
  * (and starts dropping debug printing - FIXME, eventually those prints should be encapsulated in protobufs).
  */
-class ethServerAPI : public ServerAPI<EthernetClient>
+class ethServerAPI : public ServerAPI<MeshEthernetClient>
 {
   public:
-    explicit ethServerAPI(EthernetClient &_client);
+    explicit ethServerAPI(MeshEthernetClient &_client);
 };
 
 /**
  * Listens for incoming connections and does accepts and creates instances of EthernetServerAPI as needed
  */
-class ethServerPort : public APIServerPort<ethServerAPI, EthernetServer>
+class ethServerPort : public APIServerPort<ethServerAPI, MeshEthernetServer>
 {
   public:
     explicit ethServerPort(int port);
 };
 
+void initEthApiServer(int port = SERVER_API_DEFAULT_PORT);
+void deInitEthApiServer();
+
+#if !HAS_WIFI
 void initApiServer(int port = SERVER_API_DEFAULT_PORT);
 void deInitApiServer();
+#endif
 #endif

src/mesh/wifi/WiFiAPClient.h
@@ -9,9 +9,17 @@
 #include <WiFi.h>
 #endif
 
-#if HAS_ETHERNET && defined(ARCH_ESP32)
+#if HAS_ETHERNET
+#if defined(ARCH_ESP32) && defined(ETH_PHY_TYPE)
 #include <ETH.h>
-#endif // HAS_ETHERNET
+#elif defined(USE_WS5500)
+#include <ETHClass2.h>
+#define ETH ETH2
+#elif defined(USE_CH390D)
+#include <ESP32_CH390.h>
+#define ETH CH390
+#endif
+#endif
 
 extern bool needReconnect;
 extern concurrency::Periodic *wifiReconnect;
@@ -25,7 +33,7 @@ bool isWifiAvailable();
 
 uint8_t getWifiDisconnectReason();
 
-#if defined(USE_WS5500) || defined(USE_CH390D)
+#if HAS_ETHERNET && (defined(USE_WS5500) || defined(USE_CH390D) || defined(ETH_PHY_TYPE))
 // Startup Ethernet
 bool initEthernet();
 #endif