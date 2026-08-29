e7ddccc64e6b6442979b2db357e65b539577e648
Alexander Balya <alexander.balya@gmail.com>
2026-08-29 21:45:19 +0200

    Improve InkHUD encoder keyboard navigation

--- variants/esp32s3/diy/esp32s3-e22-w5500/nicheGraphics.h
+++ variants/esp32s3/diy/esp32s3-e22-w5500/nicheGraphics.h
@@ -39,7 +39,6 @@ class E22W5500RotaryEncoder : public RotaryEncoderInterruptBase
         RotaryEncoderInterruptBase::init(E22_W5500_ROTARY_A, E22_W5500_ROTARY_B, E22_W5500_ROTARY_PRESS, INPUT_BROKER_RIGHT,
                                          INPUT_BROKER_LEFT, INPUT_BROKER_SELECT, INPUT_BROKER_SELECT_LONG, handleA, handleB,
                                          handlePress);
-        inputBroker->registerSource(this);
     }
 
   private:
@@ -52,27 +51,51 @@ class E22W5500RotaryEncoder : public RotaryEncoderInterruptBase
 
 class E22W5500InkHUDRotaryBridge : public Observer<const InputEvent *>
 {
+  public:
+    void shortPress()
+    {
+        NicheGraphics::InkHUD::InkHUD *inkhud = NicheGraphics::InkHUD::InkHUD::getInstance();
+        updateKeyboardState(inkhud);
+        inkhud->shortpress();
+        updateKeyboardState(inkhud);
+    }
+
+    void longPress()
+    {
+        NicheGraphics::InkHUD::InkHUD *inkhud = NicheGraphics::InkHUD::InkHUD::getInstance();
+        updateKeyboardState(inkhud);
+        inkhud->longpress();
+        updateKeyboardState(inkhud);
+    }
+
   protected:
     int onNotify(const InputEvent *event) override
     {
         NicheGraphics::InkHUD::InkHUD *inkhud = NicheGraphics::InkHUD::InkHUD::getInstance();
+        updateKeyboardState(inkhud);
 
         switch (event->inputEvent) {
         case INPUT_BROKER_LEFT:
-            if (systemAppletHasInput(inkhud))
+            if (keyboardActive)
+                navigateKeyboard(inkhud, false);
+            else if (systemAppletHasInput(inkhud))
                 inkhud->touchNavUp();
             else
                 inkhud->prevApplet();
             break;
         case INPUT_BROKER_RIGHT:
-            if (systemAppletHasInput(inkhud))
+            if (keyboardActive)
+                navigateKeyboard(inkhud, true);
+            else if (systemAppletHasInput(inkhud))
                 inkhud->touchNavDown();
             else
                 inkhud->nextApplet();
             break;
         case INPUT_BROKER_SELECT:
+            shortPress();
+            break;
         case INPUT_BROKER_SELECT_LONG:
-            inkhud->longpress();
+            longPress();
             break;
         default:
             break;
@@ -82,6 +105,48 @@ class E22W5500InkHUDRotaryBridge : public Observer<const InputEvent *>
     }
 
   private:
+    static constexpr uint8_t KEYBOARD_ROWS = 4;
+    static constexpr uint8_t KEYBOARD_COLUMNS = 11;
+
+    bool keyboardActive = false;
+    uint8_t keyboardRow = 0;
+    uint8_t keyboardColumn = 0;
+
+    void updateKeyboardState(NicheGraphics::InkHUD::InkHUD *inkhud)
+    {
+        bool active = inkhud->getSystemApplet("Keyboard")->handleInput;
+        if (active && !keyboardActive) {
+            keyboardRow = 0;
+            keyboardColumn = 0;
+        }
+        keyboardActive = active;
+    }
+
+    void navigateKeyboard(NicheGraphics::InkHUD::InkHUD *inkhud, bool forward)
+    {
+        NicheGraphics::InkHUD::SystemApplet *keyboard = inkhud->getSystemApplet("Keyboard");
+
+        if (forward) {
+            if (keyboardColumn < KEYBOARD_COLUMNS - 1) {
+                inkhud->navRight();
+                keyboardColumn++;
+            } else {
+                keyboard->onNavDown();
+                keyboard->onNavRight();
+                keyboardRow = (keyboardRow + 1) % KEYBOARD_ROWS;
+                keyboardColumn = 0;
+            }
+        } else if (keyboardColumn > 0) {
+            inkhud->navLeft();
+            keyboardColumn--;
+        } else {
+            keyboard->onNavUp();
+            keyboard->onNavLeft();
+            keyboardRow = (keyboardRow + KEYBOARD_ROWS - 1) % KEYBOARD_ROWS;
+            keyboardColumn = KEYBOARD_COLUMNS - 1;
+        }
+    }
+
     static bool systemAppletHasInput(const NicheGraphics::InkHUD::InkHUD *inkhud)
     {
         for (const NicheGraphics::InkHUD::SystemApplet *applet : inkhud->systemApplets) {
@@ -109,6 +174,11 @@ void setupNicheGraphics()
     InkHUD::InkHUD *inkhud = InkHUD::InkHUD::getInstance();
     inkhud->setDriver(driver);
     inkhud->setDisplayResilience(INKHUD_BUILDCONF_DISPLAYRESILIENCE);
+    inkhud->twoWayRocker = false;
+
+    inkhud->persistence->loadSettings();
+    bool saveInputSettings = !inkhud->persistence->settings.joystick.enabled || !inkhud->persistence->settings.joystick.aligned ||
+                             inkhud->persistence->settings.optionalMenuItems.nextTile;
 
     InkHUD::Applet::fontLarge = FREESANS_12PT_WIN1251;
     InkHUD::Applet::fontMedium = FREESANS_9PT_WIN1251;
@@ -117,7 +187,13 @@ void setupNicheGraphics()
     inkhud->persistence->settings.rotation = driver->height > driver->width ? 1 : 0;
     inkhud->persistence->settings.userTiles.maxCount = INKHUD_BUILDCONF_MAX_TILES;
     inkhud->persistence->settings.userTiles.count = 1;
+    inkhud->persistence->settings.joystick.enabled = true;
+    inkhud->persistence->settings.joystick.aligned = true;
+    inkhud->persistence->settings.optionalMenuItems.nextTile = false;
     inkhud->persistence->settings.optionalFeatures.batteryIcon = true;
+    inkhud->persistence->settings.joystick.alignment = (4 - inkhud->persistence->settings.rotation) % 4;
+    if (saveInputSettings)
+        inkhud->persistence->saveSettings();
 
     inkhud->addApplet("All Messages", new InkHUD::AllMessageApplet, true, true);
     inkhud->addApplet("DMs", new InkHUD::DMApplet);
@@ -131,7 +207,10 @@ void setupNicheGraphics()
     inkhud->begin();
 
 #if !MESHTASTIC_EXCLUDE_INPUTBROKER
-    // Keep encoder directions aligned with the displayed orientation.
+    // Persisted settings can override hardware input defaults during begin().
+    inkhud->persistence->settings.joystick.enabled = true;
+    inkhud->persistence->settings.joystick.aligned = true;
+    inkhud->persistence->settings.optionalMenuItems.nextTile = false;
     inkhud->persistence->settings.joystick.alignment = (4 - inkhud->persistence->settings.rotation) % 4;
 
     static E22W5500RotaryEncoder rotaryEncoder;
@@ -142,8 +221,13 @@ void setupNicheGraphics()
 
     Inputs::TwoButton *buttons = Inputs::TwoButton::getInstance();
     buttons->setWiring(0, Inputs::TwoButton::getUserButtonPin(), true);
+#if !MESHTASTIC_EXCLUDE_INPUTBROKER
+    buttons->setHandlerShortPress(0, []() { rotaryBridge.shortPress(); });
+    buttons->setHandlerLongPress(0, []() { rotaryBridge.longPress(); });
+#else
     buttons->setHandlerShortPress(0, [inkhud]() { inkhud->shortpress(); });
     buttons->setHandlerLongPress(0, [inkhud]() { inkhud->longpress(); });
+#endif
     buttons->start();
 }
 
